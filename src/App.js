42c09e26cef49b5d64e3916010be56efcecbb496
dhruv patel <d7511162@gmail.com>
2024-10-17 11:57:18 +0530

    remove unwanted

--- src/App.js
+++ src/App.js
@@ -1,38 +1,4 @@
-// // src/App.js
-// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import NavBar from "./components/NavBar"; // Import NavBar component
-// import Login from "./pages/Login";
-// import SignUp from "./pages/SignUp";
-// import ToDoApp from "./pages/ToDoApp";
-// import ExpenseTracker from "./pages/ExpenseTracker";
-// import Blog from "./pages/Blog";
-// import AddDevice from "./pages/AddDevice";
-// import Contact from "./pages/Contact";
-// import ProductCatalog from "./pages/ProductCatalog";
-// import Home from "./pages/Home"; // Import Home page if you have one
 
-// function App() {
-//   return (
-//     <Router>
-//       <NavBar /> {/* Add NavBar component */}
-//       <Switch>
-//         <Route path="/login" component={Login} />
-//         <Route path="/signup" component={SignUp} />
-//         <Route path="/todo" component={ToDoApp} />
-//         <Route path="/expense-tracker" component={ExpenseTracker} />
-//         <Route path="/blog" component={Blog} />
-//         <Route path="/add-device" component={AddDevice} />
-//         <Route path="/contact" component={Contact} />
-//         <Route path="/product-catalog" component={ProductCatalog} />
-//         <Route path="/" component={Home} exact /> {/* Default Home page */}
-//       </Switch>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-// src/App.js
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Use Routes instead of Switch
 import NavBar from "./components/NavBar"; // Import NavBar component
 import Login from "./pages/Login";
